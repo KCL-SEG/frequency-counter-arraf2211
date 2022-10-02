0c71a7a49818ca5fd31ca538cdb5b4c80b3939f2
arraf2211 <arrafrahman22@gmail.com>
2022-10-02 10:55:36 +0100

    add functionality to find repeat items in list

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,20 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+print("Enter a list of items separated by a commas: ")
+listItems = [str(value) for value in input().split(",")]
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    if items is None:
+        print("empty")
+    else:
+        for i in range(len(items)):
+            x = 0
+            for j in range(len(items)):
+                if items[i] == items[j]:
+                    x = x +1
+                    frequencies[str(items[i])] = x
+
     return frequencies
