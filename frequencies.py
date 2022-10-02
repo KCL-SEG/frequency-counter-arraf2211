e64371bc8df48697546e6cba0c1ca5e5a34742c9
arraf2211 <arrafrahman22@gmail.com>
2022-10-02 11:13:43 +0100

    corrected from tests

--- frequencies.py
+++ frequencies.py
@@ -4,14 +4,17 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    templist = []
     if items is None:
         print("empty")
     else:
-        for i in range(len(items)):
+        for y in range(len(items)):
+            templist.append(str(items[y]))
+        for i in range(len(templist)):
             x = 0
-            for j in range(len(items)):
-                if items[i] == items[j]:
+            for j in range(len(templist)):
+                if templist[i] == templist[j]:
                     x = x +1
-                    frequencies[str(items[i])] = x
-
+                    frequencies[str(templist[i])] = x
+                    
     return frequencies
